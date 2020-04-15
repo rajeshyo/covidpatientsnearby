fbda84abf85f586312d094099cc2ab0b2cd7e8b2
rajeshsaha <rajesh95mailbox@gmail.com>
2020-04-15 20:48:31 +0530

    Updates

---
+++ precache-manifest.d52808e20bcf2b8e55bd1747cf80369c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9056f8f3168ab5d11123509fa8abd6e0",
+    "url": "/covidpatientsnearby/index.html"
+  },
+  {
+    "revision": "cd0a171bc6725302f185",
+    "url": "/covidpatientsnearby/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "738b7f9a4e80c288f15d",
+    "url": "/covidpatientsnearby/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cd0a171bc6725302f185",
+    "url": "/covidpatientsnearby/static/js/2.19234a5e.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/covidpatientsnearby/static/js/2.19234a5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738b7f9a4e80c288f15d",
+    "url": "/covidpatientsnearby/static/js/main.583f733f.chunk.js"
+  },
+  {
+    "revision": "706bb612960c2871a352",
+    "url": "/covidpatientsnearby/static/js/runtime-main.7a3c15bb.js"
+  },
+  {
+    "revision": "479ca71d04b798125456be80e97f0916",
+    "url": "/covidpatientsnearby/static/media/logo.479ca71d.gif"
+  }
+]);
